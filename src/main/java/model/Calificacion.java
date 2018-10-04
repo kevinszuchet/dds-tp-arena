a5428dd2aceadc1144b03dea9a6691e1eddbe088
fedekiwo <fedekiwo@gmail.com>
2018-10-03 22:50:45 -0300

    Rollback

--- src/main/java/model/Calificacion.java
+++ src/main/java/model/Calificacion.java
@@ -1,10 +1,5 @@
 package model;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
-import json.CalificacionParser;
-
-//@JsonDeserialize(using = CalificacionParser.class)
 public interface Calificacion {	
 	public String getCalificacion();
 	public boolean estaAprobada();
